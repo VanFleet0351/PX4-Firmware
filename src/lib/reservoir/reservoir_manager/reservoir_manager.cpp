4250d7d896bf718552f8f2eaec6aec60fd068f43
Mohamed Ahmed <mohamed.nurhussien@gmail.com>
2020-03-29 13:23:30 -0400

    Created reservoir manager

---
+++ src/lib/reservoir/reservoir_manager/reservoir_manager.cpp
@@ -0,0 +1,43 @@
+
+
+#include "reservoir_manager.hpp"
+
+reservoir_manager::reservoir_manager(uint8_t input_vector_size, uint16_t reservoir_size,
+                                       uint8_t output_vector_size, double sparsity, double spectral_radius,
+                                       double leakage_rate, double reg_param, double washout)
+        : sparsity_(sparsity), spectral_radius_(spectral_radius), leakage_rate_(leakage_rate),
+          regression_parameter_(reg_param), washout_(washout),  input_dimension_(input_vector_size),
+          reservoir_dimension_(reservoir_size), output_dimension_(output_vector_size){
+
+	  }
+void reservoir_manager::create_resrvoirs(){
+	reservoirs.push_back(new reservoir_computer(
+		input_dimension_,  reservoir_dimension_, output_dimension_,
+		sparsity_, spectral_radius_, leakage_rate_, regression_parameter_, washout_ ));
+
+}
+
+void reservoir_manager::train_reservoirs(){
+	list <int> :: iterator it;
+    	for(it = reservoirs.begin(); it != reservoirs.end(); ++it){
+		if(*it.get_reservoir_status() == NOT_TRAINED){
+			*it.train();
+		}
+	}
+}
+
+void reservoir_manager::destroy_reservoirs(){
+	list <int> :: iterator it;
+    	for(it = reservoirs.begin(); it != reservoirs.end(); ++it){
+		*it.reset();
+	}
+}
+
+void reservoir_manager::show_status(){
+	list <int> :: iterator it;
+	int i=1;
+    	for(it = reservoirs.begin(); it != reservoirs.end(); ++it){
+		cout <<"Reservoir " << i <<" :" << *it.get_reservoir_status(); //PX4_INFO_RAW("")
+		i++;
+	}
+}
---
+++ src/lib/reservoir/reservoir_manager/reservoir_manager.hpp
@@ -0,0 +1,43 @@
+
+#ifndef PX4_RESERVOIR_MANAGER_HPP
+#define PX4_RESERVOIR_MANAGER_HPP
+
+#include "reservoir_computer.hpp"
+#include <iostream>
+#include <list>
+#include <iterator>
+using namespace std;
+
+class reservoir_manager(){
+	public:
+	explicit reservoir_manager(uint8_t input_vector_size, uint16_t reservoir_size,
+                                uint8_t output_vector_size, double sparsity, double spectral_radius,
+                                double leakage_rate, double reg_param, double washout);
+
+	list<reservoir_computer> reservoirs;
+	void create_reservoir();
+
+	void destroy_reservoirs();
+	void train_reservoirs();
+	void show_status();
+
+
+
+
+
+	private:
+	//hyperparameters
+	double sparsity_; // k in Canaday's paper usually around 10%
+	double spectral_radius_; // rho. 1.0 is a good starting point per thesis
+	double leakage_rate_; //gamma for Wendson, a for Canaday. I've seen this set to 0.3, but canaday replaced it with h/c. thesis page 20 & 21
+	double regression_parameter_; //alpha. Canaday has this at 1e-6, I've seen others use 1e-8
+	double washout_;
+
+	uint8_t input_dimension_;
+	uint16_t reservoir_dimension_;
+	uint8_t output_dimension_;
+
+
+
+}
+
